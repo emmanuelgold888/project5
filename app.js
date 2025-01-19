3c86d6dbfc6eb3f87a9b3f42759468f5eaca9a6e
emmanuelgold888 <emmanuelfranck551@gmail.com>
2025-01-19 18:21:11 +0100

    Update app.js

--- app.js
+++ app.js
@@ -5,3 +5,15 @@ const camerView = document.querySelector("#camera--view"),
       cameraOutput = document.querySelector("#camera--output"),
       cameraSensor = document.querySelector("#camera--sensor"),
       cameraTrigger = document.querySelector("#camera--trigger")
+
+function cameraStart(){
+      navigator.mediaDevices
+            .getUserMedia(constraints)
+            .then(function(stream) {
+            track = stream.getTracks()[0];
+            cameraView.srcObject = stream;
+            })
+            .catch(function(error) {
+                  console.error("Oops. Something is broken.", error);
+            });
+}
