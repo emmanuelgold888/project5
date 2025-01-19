3da2500c7fc5def9a2a23587fb0c0d198d7e6f83
emmanuelgold888 <emmanuelfranck551@gmail.com>
2025-01-19 18:12:23 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 
+var constraints = { video: { facingMode: "user" }, audio: false };
+
+const camerView = document.querySelector("#camera--view"),
+      cameraOutput = document.querySelector("#camera--output"),
+      cameraSensor = document.querySelector("#camera--sensor"),
+      cameraTrigger = document.querySelector("#camera--trigger")
