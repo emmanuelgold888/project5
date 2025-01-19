2d90d2d22c1c02368a01617b7b78bdcd449c00bd
emmanuelgold888 <emmanuelfranck551@gmail.com>
2025-01-19 19:04:31 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,34 +1,97 @@
-// Set constraints for the video stream
-var constraints = { video: { facingMode: "user" }, audio: false };
+"use strict";
+var video = document.getElementById('video');
+var canvas = document.getElementById('canvas');
+var videoStream = null;
+var preLog = document.getElementById('preLog');
 
-// Define constants
-const camerView = document.querySelector("#camera--view"),
-      cameraOutput = document.querySelector("#camera--output"),
-      cameraSensor = document.querySelector("#camera--sensor"),
-      cameraTrigger = document.querySelector("#camera--trigger");
-
-//Access the device camera and stream to cameraView
-function cameraStart(){
-      navigator.mediaDevices
-            .getUserMedia(constraints)
-            .then(function(stream) {
-            track = stream.getTracks()[0];
-            cameraView.srcObject = stream;
-            })
-            .catch(function(error) {
-                  console.error("Oops. Something is broken.", error);
-            });
+function log(text)
+{
+	if (preLog) preLog.textContent += ('\n' + text);
+	else alert(text);
 }
 
-// Take a picture when cameraTrigger is tapped
-cameraTrigger.onclick = function(){
-      cameraSensor.width = cameraView.videoWidth;
-      cameraSensor.height = cameraView.videoHeight;
-
-      cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
-      cameraOutput.src = cameraSensor.toDataURL("image/webp");
-      cameraOutput.classList.add("taken");
+function snapshot()
+{
+	canvas.width = video.videoWidth;
+	canvas.height = video.videoHeight;
+	canvas.getContext('2d').drawImage(video, 0, 0);
 }
 
-// Start the video stream when the window loads
-window.addEventListener("load", cameraStart, false);
+function noStream()
+{
+	log('Access to camera was denied!');
+}
+
+function stop()
+{
+	var myButton = document.getElementById('buttonStop');
+	if (myButton) myButton.disabled = true;
+	myButton = document.getElementById('buttonSnap');
+	if (myButton) myButton.disabled = true;
+	if (videoStream)
+	{
+		if (videoStream.stop) videoStream.stop();
+		else if (videoStream.msStop) videoStream.msStop();
+		videoStream.onended = null;
+		videoStream = null;
+	}
+	if (video)
+	{
+		video.onerror = null;
+		video.pause();
+		if (video.mozSrcObject)
+			video.mozSrcObject = null;
+		video.src = "";
+	}
+	myButton = document.getElementById('buttonStart');
+	if (myButton) myButton.disabled = false;
+}
+
+function gotStream(stream)
+{
+	var myButton = document.getElementById('buttonStart');
+	if (myButton) myButton.disabled = true;
+	videoStream = stream;
+	log('Got stream.');
+	video.onerror = function ()
+	{
+		log('video.onerror');
+		if (video) stop();
+	};
+	stream.onended = noStream;
+	if (window.webkitURL) video.src = window.webkitURL.createObjectURL(stream);
+	else if (video.mozSrcObject !== undefined)
+	{//FF18a
+		video.mozSrcObject = stream;
+		video.play();
+	}
+	else if (navigator.mozGetUserMedia)
+	{//FF16a, 17a
+		video.src = stream;
+		video.play();
+	}
+	else if (window.URL) video.src = window.URL.createObjectURL(stream);
+	else video.src = stream;
+	myButton = document.getElementById('buttonSnap');
+	if (myButton) myButton.disabled = false;
+	myButton = document.getElementById('buttonStop');
+	if (myButton) myButton.disabled = false;
+}
+
+function start()
+{
+	if ((typeof window === 'undefined') || (typeof navigator === 'undefined')) log('This page needs a Web browser with the objects window.* and navigator.*!');
+	else if (!(video && canvas)) log('HTML context error!');
+	else
+	{
+		log('Get user media…');
+		if (navigator.getUserMedia) navigator.getUserMedia({video:true}, gotStream, noStream);
+		else if (navigator.oGetUserMedia) navigator.oGetUserMedia({video:true}, gotStream, noStream);
+		else if (navigator.mozGetUserMedia) navigator.mozGetUserMedia({video:true}, gotStream, noStream);
+		else if (navigator.webkitGetUserMedia) navigator.webkitGetUserMedia({video:true}, gotStream, noStream);
+		else if (navigator.msGetUserMedia) navigator.msGetUserMedia({video:true, audio:false}, gotStream, noStream);
+		else log('getUserMedia() not available from your Web browser!');
+	}
+}
+
+start();
